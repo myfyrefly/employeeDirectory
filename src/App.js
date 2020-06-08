023f62641d03788a310403dfbac97cca4a47aa43
sofiaM <smejia03@gmail.com>
2020-06-08 12:07:25 -0500

    sample employees.json rendering to page

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import EmployeeCard from "./components/EmployeeCard";
+import Wrapper from "./components/Wrapper";
+import Title from "./components/Title";
+import employees from "./employees.json";
+import SortButton from "./components/sortBtn";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  // Setting this.state.employees to the employees json array
+  state = {
+    employees
+  };
+
+
+  // Map over this.state.employees and render a EmployeeCard component for each employee object
+  render() {
+    return (
+      
+      <Wrapper>
+        <Title>Employee Directory</Title>
+        <SortButton></SortButton>
+        {this.state.employees.map(employee => (
+          <EmployeeCard
+            removeFriend={this.removeFriend}
+            id={employee.id}
+            key={employee.id}
+            name={employee.name}
+            image={employee.avatar}
+            email={employee.email}
+            role={employee.role}
+            location={employee.location}
+          />
+        ))}
+      </Wrapper>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/sortBtn/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function SortButton() {
+  return (
+    <div>
+      <button className="primary">Test Button</button>
+    </div>
+  );
+}
+
+export default SortButton;
